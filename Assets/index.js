d805458a32fde7b6ebf9cdd3d1f02df8eee984a4
Jayad Arshad <jayadusama@hotmail.co.uk>
2022-05-13 20:48:23 +0100

    start button click

--- Assets/index.js
+++ Assets/index.js
@@ -4,9 +4,23 @@ const startButton = document.getElementById("start-btn");
 //target banner section
 const bannerSection = document.getElementById("banner")
 
+// function to render question to page
+const renderQuestion = () => {
+    console.log("render question")
+}
+
+// function to remove banner from page
+const removeBanner = () => {
+    console.log("remove banner")
+}
 //declare event handler function for start buttonclick
 const handleStartButtonClick = (event) => {
   console.log("Start button clicked");
+
+  // remove banner section
+removeBanner();
+  //render the qs
+renderQuestion();
 };
 
 // add event listener to start button
